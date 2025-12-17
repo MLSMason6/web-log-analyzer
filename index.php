b3a9bdcfbdadfbd0b3968881b1c629437d940e86
mlsmason6 <mlsmason12@gmail.com>
2025-12-17 11:03:32 -0600

    index

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Log Analyzer</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+    
+    <h1>🔍 Web Log Analyzer</h1>
+    <p>Upload an Apache or Nginx access log to analyze traffic and detect suspicious activity.</p>
+
+    <form action="analyze.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="logfile" accept=".log,.txt" required>
+        <br><br>
+        <button type ="submit">Analyze Log</button>
+    </form>
     
 </body>
 </html>
